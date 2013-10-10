b933108091504e32a782ae1bb6b871d82e7b9237
Abhishek Jain <abhi2000@gmail.com>
2013-10-10 22:13:26 +0100

    Initial Commit

---
+++ KnockOutDemos/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KnockOutDemos.Models;
+
+namespace KnockOutDemos.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult HelloWorld()
+        {
+            return View();
+        }
+
+        public ActionResult ViewUser()
+        {
+            return View();
+        }
+
+        public ActionResult RandomUsers()
+        {
+/*
+            var initialState = new[] {
+                    new RandomUser { Title = "Mr", FirstName = "Gabriella", LastName = "Cook", Street = "1303 w dallas st", City = "Waxahachie", State  = "North carolina", Gender = "F", 
+                        Zip = "63145", ImageUrl = "http://api.randomuser.me/0.2/portraits/women/34.jpg"}, 
+                        new RandomUser { Title = "Mr", FirstName = "Gabriella", LastName = "Cook", Street = "1303 w dallas st", City = "Waxahachie", State  = "North carolina", Gender = "F", 
+                        Zip = "63145", ImageUrl = "http://api.randomuser.me/0.2/portraits/women/34.jpg"},
+                };
+*/
+            return View();
+
+        }
+
+
+    }
+}
---
+++ KnockOutDemos/Models/RandomUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KnockOutDemos.Models
+{
+    public class RandomUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public string Gender { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
